9161087f093562726b414b717f0b06e3e433fc7a
elpsychicmustache <github@psychicmustache.com>
2025-11-22 21:19:05 -0700

    Created directory_navigatory.py to handle the interaction between the spiderman.py and directory_asset.py - currently not implemented

---
+++ src/directory_navigator.py
@@ -0,0 +1,13 @@
+import subprocess
+
+from directory_asset import DirectoryAsset
+
+class DirectoryNavigator:
+    def __init__(self, current_directory:DirectoryAsset) -> None:
+        self.current_directory = current_directory
+
+
+
+
+
+
