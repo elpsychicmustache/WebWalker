56d8c203dbe76479cd90dcd7f5bc4ce46ef1591f
elpsychicmustache <github@psychicmustache.com>
2025-11-30 20:18:22 -0700

    Updated docstrings and some comments in spiderman.py

--- src/spiderman.py
+++ src/spiderman.py
@@ -11,20 +11,27 @@ from directory_navigator import DirectoryNavigator
 def main(stdscr) -> None:
     """Running this starts the program.
 
-    It grabs the input file, which should be '../data/input.txt' if default option for --input_file is used.
-    Using this file, it creates the root directory, and then populates children directories from the input file.
+    Running without any arguments causes the program to grab the input file,
+    which shoud be '../data/input.txt'
+    Using the above file, the program creates the root directory as '/' and then populates children
+    directories that are specified in the aforementioned input file (which creates a directory tree).
+    Lastly, it creates the DirectoryNavigator object, which starts a loop that allows the user
+    to interact with the directory tree.
 
-    It then creates the DirectoryNavigator object, which then starts the main loop.
+    Other arguments can be provided which will change what happens. Run -h to learn more.
+
+    :param stdscr: The curses instantiation from curses.wrapper()
+    :stdscr type: curses.window
     """
     args:argparse.Namespace = get_argparse()
     input_file:str = args.input_file
     root_directory_name:str = args.root_directory
     output_file:str = args.output_file
 
-    project_root = get_parent_path()
+    project_root:"PosixPath" = get_parent_path()
     directories:str = None
 
-    # if input_file was provided, grab the data
+    # if input_file was provided, populate directories with the data
     if input_file:
         input_file = project_root / "data" / input_file
 
@@ -36,15 +43,13 @@ def main(stdscr) -> None:
 
     # if output_file was provided, then generate outputfile. Otherwise run main loop.
     if output_file:
-        print("Inside of output_file running")
-        print(f"Trying to save to {output_file}")
         main_directory_asset.create_output_file(output_file_name=output_file)
     else:
         navigator = DirectoryNavigator(main_directory_asset, stdscr)
 
 
 def get_argparse() -> argparse.Namespace:
-    """Grabs the command-line arguments.
+    """Grabs the command-line arguments specified when the program was evoked.
 
     :returns: The arguments given when starting the program.
     :rtype: argparse.Namespace
@@ -86,12 +91,18 @@ def get_argparse() -> argparse.Namespace:
 
     args = parser.parse_args()
 
+    # Checking and updating parameters based on what switches and arguments were provided.
     check_args_combinations(args)
 
     return args
 
 
-def check_args_combinations(args) -> None:
+def check_args_combinations(args:argparse.Namespace) -> None:
+    """Checks args and performs validation, error raising, or other logic problems for args.
+
+    :param args: The arguments grabbed from argparse.
+    :type args: argparse.Namespace
+    """
     if args.empty_directory:
         args.input_file = None
         args.output_file = None
@@ -99,6 +110,8 @@ def check_args_combinations(args) -> None:
 
 def get_parent_path() -> "PosixPath":
     """Grabs the root directory of the project.
+
+    This is needed to get to the data folder and other needed resources.
 
     :returns: The path to the project's directory.
     :rtype: PosixPath
@@ -108,9 +121,13 @@ def get_parent_path() -> "PosixPath":
     return project_root
 
 
-def instantiate_directory_object(parent_directory_name, directory_list) -> DirectoryAsset:
-    """Creates the root directory and populates it's children.
+def instantiate_directory_object(parent_directory_name:str, directory_list:str) -> DirectoryAsset:
+    """Creates the 'root' directory_asset and populates it's children if provided.
 
+    :param parent_directory_name: The name of the root directory.
+    :type parent_directory_name: str
+    :param directory_list: The string object containing a list of children from walkman.js.
+    :type directory_list: str
     :returns: The root directory that is populated with children directories.
     :rtype: DirectoryAsset
     """
@@ -127,4 +144,5 @@ if __name__ == "__main__":
     # This way, help can be ran before curses takes over stdout.
     # Args will also be called inside of main.
     args = get_argparse()
+
     curses.wrapper(main)
