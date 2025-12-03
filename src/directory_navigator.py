6e8097a0c01fe6bc53d505e6bc0a92e46f45dab1
elpsychicmustache <github@psychicmustache.com>
2025-12-02 20:13:54 -0700

    Refactored directory_navigator to utilize a method called show_banner(). Maybe not the best approach, but it guesses what the message to display is based on the y and x values.

--- src/directory_navigator.py
+++ src/directory_navigator.py
@@ -41,6 +41,36 @@ class DirectoryNavigator:
         # starts an 'infinite' loop
         self.enter_main_loop()
 
+    def show_banner(self, y:int=0, x:int=0, message:str=None, reverse:bool=True) -> int:
+        """Show a banner to stdout to user.
+
+        This method is more of a helper method, and standardizes some of the banners, such as header and exit banners. If a message is not provided,
+        it tries to best guess what the message to display is. If y and x are 0, then it shows the header banner. If y is of any value other value,
+        then it informs the user to press enter to quit.
+
+        :param y: The line to which display the message. DEFAULTS to 0.
+        :type y: int
+        :param x: The column to display the message. DEFAULTS to 0.
+        :type x: int
+        :param message: The message to display to the user. DEFAULTS to None.
+        :type message: str
+        :param reverse: If the message should display in reverse colors. DEFAULTS to True.
+        :type reverse: bool
+        :returns: The length of the message.
+        :rtype: int
+        """
+        if message:
+            self.stdscr.addstr(y, x, message, curses.A_REVERSE if reverse else curses.A_NORMAL)
+        else:
+            # Attempt to best guess what message the user wants to display:
+            if y==0 and x==0:
+                message = f"[+] Currently in '{self.current_directory.name}': {len(DirectoryAsset.master_list)} directories exist"
+            else:
+                message = "Press ENTER ... "
+            self.stdscr.addstr(y, x, message, curses.A_REVERSE if reverse else curses.A_NORMAL)
+
+        return len(message)
+
     def create_options_menu(self) -> dict[int, tuple]:
         """Calling this method creates the main menu options as a dictionary.
 
@@ -97,8 +127,7 @@ class DirectoryNavigator:
 
     def show_main_menu(self) -> int:
         """Displays the main menu options."""
-        banner = f"[+] Currently in '{self.current_directory.name}': {len(DirectoryAsset.master_list)} directories exist"
-        self.stdscr.addstr(0,0, banner, curses.A_REVERSE)
+        self.show_banner()
 
         current_display_line:int = 1
 
@@ -140,25 +169,22 @@ class DirectoryNavigator:
                 # Printing the last directory that can be shown here; otherwise, we lose 1 directory every refresh
                 # due to the else statement still incrementing index
                 self.stdscr.addstr(current_print_line, 0, directory_list[index])
-                self.stdscr.addstr(last_available_line, 0, 
-                                   f"{index + 1} out of {len(directory_list)} directories printed. Print any key to continue or q to quit.", 
-                                   curses.A_REVERSE
-                                   )
+
+                col_length:int = self.show_banner(y=last_available_line, x=0,
+                                                  message=f"{index + 1} out of {len(directory_list)} directories printed. Print any key to continue or q to quit."
+                                                  )
                 self.stdscr.refresh()
                 current_print_line = 0
                 # If user's key input is q, then quit
-                if self.stdscr.getkey().lower() == "q":
+                if self.stdscr.getkey(last_available_line, col_length).lower() == "q":
                     self.stdscr.clear()
                     return
                 self.stdscr.clear()
 
-        self.stdscr.addstr(last_available_line, 0,
-                           f"All directories printed. Press any key to exit.",
-                           curses.A_REVERSE
-                           )
+        col_length:int = self.show_banner(y=last_available_line, x=0, message=f"All directories printed. Press any key to exit.")
 
         self.stdscr.refresh()
-        self.stdscr.getch()
+        self.stdscr.getch(last_available_line, col_length)
 
     def populate_child_directory(self) -> None:
         """Calling this method evokes the populate_child_directories() for the current_directory attribute.
@@ -171,20 +197,21 @@ class DirectoryNavigator:
 
         # This block of code grabs and handles getting the child's name.
         input_banner = "[+] Please enter the name of the child directory: "
-        self.stdscr.addstr(0, 0, input_banner)
-        child_name:str = self.stdscr.getstr(0, len(input_banner)).decode()
+        col_length = self.show_banner(message=input_banner, reverse=False)
+        child_name:str = self.stdscr.getstr(0, col_length).decode()
 
         if not self.current_directory.children.get(child_name):
             self.stdscr.clear()
             self.stdscr.addstr(0, 0, f"[!] {child_name} is not a valid child directory. Nothing happened ...", self.RED_ALERT)
             # TODO: Ask if user wants to create the directory. Before doing so, the directory needs to be checked against the master list.
-            self.stdscr.getch(1, 0)
+            col_length = self.show_banner(y=1, x=0)
+            self.stdscr.getch(1, col_length)
             return  # end this function
 
         # This block of code grabs the input file name.
         file_input_banner = "[+] Please enter the name of the file from which to populate the directory: "
-        self.stdscr.addstr(1, 0, file_input_banner)
-        file_name:str = self.stdscr.getstr(1, len(file_input_banner)).decode()
+        col_length = self.show_banner(1, 0, message=file_input_banner, reverse=False)
+        file_name:str = self.stdscr.getstr(1, col_length).decode()
 
         # Tell the user the file was found and children populated, or file was not found.
         try:
@@ -197,9 +224,8 @@ class DirectoryNavigator:
             self.stdscr.clear()
             self.stdscr.addstr(0, 0, f"[+] {child_name} directories have been populated!", self.GREEN_ALERT)
         finally:
-            closing_message = "Press ENTER ..."
-            self.stdscr.addstr(1, 0, closing_message)
-            self.stdscr.getch(1, len(closing_message))
+            col_length = self.show_banner(y=1, x=0)
+            self.stdscr.getch(1, col_length)
 
     def add_child_directory(self) -> None:
         """Creates a single child directory to current_directory.
@@ -209,27 +235,25 @@ class DirectoryNavigator:
         """
         # Show the user what directory they are in.
         self.stdscr.clear()
-        banner = f"[+] Currently in '{self.current_directory.name}': {len(DirectoryAsset.master_list)} directories exist"
-        self.stdscr.addstr(0,0, banner, curses.A_REVERSE)
+        self.show_banner()
 
         input_message = "Please enter the child's name: "
-        self.stdscr.addstr(1, 0, input_message)
+        col_length = self.show_banner(y=1, x=0, message=input_message, reverse=False)
+        child_name = self.stdscr.getstr(1, col_length).decode()
 
-        child_name = self.stdscr.getstr(1, len(input_message)).decode()
         try:
             child = DirectoryAsset(name=child_name, parent=self.current_directory, level=self.current_directory.level+2)
         except ValueError as e:
             self.stdscr.addstr(2, 0, str(e), self.RED_ALERT)
             closing_message = "Nothing happened. Press ENTER ..."
-            self.stdscr.addstr(3, 0, closing_message)
-            self.stdscr.getch(3, len(closing_message))
+            col_length = self.show_banner(y=3, x=0, message=closing_message)
+            self.stdscr.getch(3, col_length)
         else:
             self.current_directory.add_child(child)
 
             self.stdscr.addstr(2, 0, f"[+] {child_name} has been added to {self.current_directory.name}", self.GREEN_ALERT)
-            closing_message = "Press ENTER ..."
-            self.stdscr.addstr(3, 0, closing_message)
-            self.stdscr.getch(3, len(closing_message))
+            col_length = self.show_banner(y=3, x=0)
+            self.stdscr.getch(3, col_length)
 
     def change_directory(self) -> None:
         """Changes the current_directory attribute.
@@ -237,9 +261,10 @@ class DirectoryNavigator:
         Nothing will happen if the directory being called does not exist in the master_list of DirectoryAsset.
         """
         self.stdscr.clear()
+        self.show_banner()
         new_directory_prompt = "[+] Please enter the name of the directory to change to: "
-        self.stdscr.addstr(0, 0, new_directory_prompt)
-        new_directory_name = self.stdscr.getstr(0, len(new_directory_prompt)).decode()
+        col_length = self.show_banner(y=1, x=0, message=new_directory_prompt, reverse=False)
+        new_directory_name = self.stdscr.getstr(1, col_length).decode()
 
         try:
             directory_location:int = [x.name for x in DirectoryAsset.master_list].index(new_directory_name)
@@ -251,9 +276,8 @@ class DirectoryNavigator:
             self.stdscr.clear()
             self.stdscr.addstr(0, 0, f"[+] Successfully changed to '{new_directory_name}'.", self.GREEN_ALERT)
 
-        closing_message = "Press ENTER ..."
-        self.stdscr.addstr(1, 0, closing_message)
-        self.stdscr.getch(1, len(closing_message))
+        col_length = self.show_banner(y=1, x=0)
+        self.stdscr.getch(1, col_length)
 
     def save_directory(self) -> None:
         """Save the current directory tree to a file.
@@ -265,12 +289,11 @@ class DirectoryNavigator:
         self.stdscr.clear()
 
         # Show user current directory information.
-        banner = f"[+] Currently in '{self.current_directory.name}': {len(DirectoryAsset.master_list)} directories exist"
-        self.stdscr.addstr(0,0, banner, curses.A_REVERSE)
+        self.show_banner()
 
         input_banner = "[+] Please enter the name of the output file (default 'output.txt'): "
-        self.stdscr.addstr(1, 0, input_banner)
-        file_name = self.stdscr.getstr(1, len(input_banner)).decode()
+        col_length = self.show_banner(1, 0, input_banner, reverse=False)
+        file_name = self.stdscr.getstr(1, col_length).decode()
 
         if file_name:
             self.stdscr.addstr(2, 0, f"Saving to data/{file_name}...")
@@ -281,9 +304,8 @@ class DirectoryNavigator:
             self.current_directory.create_output_file()
             self.stdscr.addstr(3, 0, f"[+] Saved to data/outputfile.txt", self.GREEN_ALERT)
 
-        closing_banner = "Press ENTER ..."
-        self.stdscr.addstr(4, 0, closing_banner)
-        self.stdscr.getch(4, len(closing_banner))
+        col_length = self.show_banner(y=4, x=0)
+        self.stdscr.getch(4, col_length)
 
     def quit_program(self) -> None:
         """Clears the screen.
